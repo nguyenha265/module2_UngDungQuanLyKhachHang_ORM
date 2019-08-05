96ecca8aa23d46c2cdc6d145fa2a7b5bae506830
Nguyenha <you@example.com>
2019-08-05 17:09:41 +0700

    thuc hanh

---
+++ src/main/java/com/codegym/service/Service.java
@@ -0,0 +1,13 @@
+package com.codegym.service;
+
+import java.util.List;
+
+public interface Service<T> {
+    List<T> findAll();
+
+    T findById(Long id);
+
+    void save(T model);
+
+    void remove(Long id);
+}
